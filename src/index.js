b17d492ab3eda926ccf6106428064c6557901ad8
hiddencilantro <stevekim1109@gmail.com>
2021-07-23 16:52:20 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  function addToList() {
+    let ul = document.getElementById('tasks');
+    let li = document.createElement('li');
+    let input = document.getElementById('new-task-description').value;
+    li.textContent = input;
+    ul.appendChild(li);
+  }
+
+  document.querySelector('input[type="submit"]').addEventListener('click', function(e) {
+    addToList();
+    e.preventDefault();
+  });
 });
